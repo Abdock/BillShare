8cd4f95f77e58b58e188ea9c76dc714f2835d98e
Abdussattar Kassymbekov <a.kassymbekov@akvelon.com>
2023-02-11 17:16:26 +0600

    Add exception handler

--- BillShare/Program.cs
+++ BillShare/Program.cs
@@ -8,6 +8,8 @@ builder.Services.ConfigureCors();
 builder.Services.ConfigureAuthenticationAndAuthorization(builder.Configuration);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
 
 var app = builder.Build();
 if (app.Environment.IsDevelopment())
@@ -21,5 +23,6 @@ app.UseRouting();
 app.UseCors(CorsProfiles.AllowsAll);
 app.UseAuthentication();
 app.UseAuthorization();
+app.UseCustomExceptionHandler();
 app.MapControllers();
 app.Run();
